344fc62c4f2594f5657250e026b3a0cf5dcee8e7
viktorvasilev-web <viktorsfsl@gmail.com>
2025-08-07 20:58:12 +0300

    Update accountfilter.js

--- JS/ui/accountfilter.js
+++ JS/ui/accountfilter.js
@@ -2,7 +2,7 @@ const delegationMap = {
     "0x9218f8aa46faae8081d7a2d1e20bd135fcf76aea": "3 Savana Main NM FARM-ViktorAccount1",
     "0xf5b3b8a580778d8723fec48936d6d7c6b1e0f995": "ViktorAccount2",
     "0xb3296808909ea5a910666ecdc1fef1a847d52237": "Kazima",
-    "0x61272ff9db3da124029d29fefa3dd773d8f67a74": "9-9",
+    "0xe47c02a904b1401f49cef081eef2051423957022": "Account 44 | Projects Eri",
     "0x037dc10b4c530e9a0735249d0f41efc25f727912": "Metis | Cryptosis",
     "0xb9bb961a410117b32f50c997dd68d0a68f0a8a53": "TOYOIN 9",
     "0x6416c37a5ae79c82d5c447f3e75976fdeffa377e": "Chaechae",
